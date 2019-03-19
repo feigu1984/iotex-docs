585417cb87dc92049b76cf3212d92bfb6d58e2df
feiren.kuang <feigu_1984@qq.com>
2019-03-19 13:39:28 +0800

    debug

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:iotexproject/iotex-docs.git master:gh-pages
+ git push  git@github.com:feigu1984/iotex-docs.git master:gh-pages
 
 cd -
